8b0fdedabafea9b733db7622f0a83276edc0583c
showyouhappiness <41782506+showyouhappiness@users.noreply.github.com>
2020-08-11 15:18:00 +0800

    修改点击事件  触发的cells的bug

--- mergeCells2.js
+++ mergeCells2.js
@@ -3,7 +3,7 @@ function mergeCell(tableId, col) {
     for (var i = 1; i < tr.rows.length; i++) { //表示数据内容的第二行
         if (tr.rows[i].cells[col].innerHTML === tr.rows[i - 1].cells[col].innerHTML) { //col代表列
             t = i - 1;
-            while (tr.rows[i].cells[col].innerHTML === tr.rows[t].cells[col].innerHTML) {
+            while (tr.rows[i]!=null&&tr.rows[i].cells[col].innerHTML === tr.rows[t].cells[col].innerHTML) {
                 tr.rows[i].cells[col].style.display = "none";
                 if (tr.rows[t].cells[col].rowSpan <= (i - t)) {
                     tr.rows[t].cells[col].rowSpan += 1; //设置前一行的rowspan+1
